84da3c10fc6c09de65a2e3684cfe3b77719657c9
761394 <761394@PC421074.cts.com>
2019-11-20 14:30:09 +0530

    blogtile commit

---
+++ trainingBatch.trainingBatch/core/src/main/java/trainingBatch/core/models/Multifield.java
@@ -0,0 +1,23 @@
+package trainingBatch.core.models;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+ 
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional; 
+import org.apache.sling.settings.SlingSettingsService;
+ 
+@Model(adaptables = Resource.class)
+public class Multifield {
+ 
+    // Inject the products node under the current node
+    @Inject
+    @Optional
+    public Resource products;
+ 
+    // No need of a post construct as we don't have anything to modify after the
+    // model is constructed
+}
